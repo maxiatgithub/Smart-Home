7495a8fbdaace3cbc4699c06e5af73778795bceb
Maximilian M <maxserver@outlook.de>
2018-02-23 21:34:41 +0100

    Detaillierte Kommentare wurden hinzugefügt

--- KI test 01/Program.cs
+++ KI test 01/Program.cs
@@ -11,44 +11,45 @@ namespace KI_test_01
 {
     class Program
     {
-        //Sprachwiedergabe
-        SpeechSynthesizer s = new SpeechSynthesizer();
-        //Sprachwiedergabe
-        SpeechRecognitionEngine h = new SpeechRecognitionEngine();
+
+        SpeechSynthesizer s = new SpeechSynthesizer(); //Instanz zur Sprachwiedergabe
+        SpeechRecognitionEngine h = new SpeechRecognitionEngine(); //Instanz zur Spracherkennung
+
         // Erzeugung der Befehls- und Antwortliste
         static List<string> Befehle = new List<string>() { };
         static List<string> Answers = new List<string>() { };
 
+
         public void initialisation(List<string> Befehle)
         {
-            Choices commands = new Choices();
-            commands.Add(Befehle.ToArray());
-            GrammarBuilder gbuilder = new GrammarBuilder();
-            gbuilder.Append(commands);
-            Grammar grammar = new Grammar(gbuilder);
-            h.LoadGrammar(grammar);
-            h.SetInputToDefaultAudioDevice();
-            h.SpeechRecognized += recEngine_SpeechRecognized;
-            h.RecognizeAsync(RecognizeMode.Multiple);
-            s.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+            Choices commands = new Choices(); //Instanz zur Kommandoerkennung
+            commands.Add(Befehle.ToArray()); //Gibt der Instanz zur Kommandoerkennung die Befehlsliste über
+            GrammarBuilder gbuilder = new GrammarBuilder(); //Instanz zur Grammatikanalyse
+            gbuilder.Append(commands); //Gibt der Instanz zur Grammatikanalyse die Befehlsliste über
+            Grammar grammar = new Grammar(gbuilder); //Gibt die Instanz zur Grammatikanalyse weiter zur Erzeugung einer Instanz, die die neue Grammatik darstellt
+            h.LoadGrammar(grammar); //Die Instanz zur Spracherkennung (Zeile 16) lernt nun die neue Grammatik
+            h.SetInputToDefaultAudioDevice(); //Als Input wird das Standart Aufnahmegerät ausgewählt
+            h.SpeechRecognized += recEngine_SpeechRecognized; //Sobald die Spracherkennung ein Wort erkannt hat, wird die Event Methode ausgeführt
+            h.RecognizeAsync(RecognizeMode.Multiple); //Die Spracherkennung soll asynchron stattfinden und mehrere Eingaben hintereinander verarbeiten können
+            s.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult); //Sprachsample Auswahl zur Sprachwiedergabe
             Console.WriteLine("Sag ruhig was");
         }
 
         public static void Main(string[] args)
         {
-            Program Start = new Program();
-         // Pfad zur Textdatei für die Auflistung aller Befehle und legt neuen IO StreamReader an zur Auslesung der Textdatei
-            String path = @"G:\Visual Studio Enterprise codes\KI test 01\KI test 01\commandData.txt";
+            Program Start = new Program();  
+            String path = @"G:\Visual Studio Enterprise codes\KI test 01\KI test 01\commandData.txt"; // Pfad zur Textdatei für die Auflistung aller Befehle und legt neuen IO StreamReader an zur Auslesung der Textdatei
             System.IO.StreamReader file = new System.IO.StreamReader(path);
 
 
-            getCommands(file.ReadLine(),Befehle, Answers, file);
-            Start.initialisation(Befehle);
+            getCommands(file.ReadLine(),Befehle, Answers, file); //Erzeugen der beiden Listen (Befehl und Antwort)
+            Start.initialisation(Befehle); //Hier habe ich alles zusammengefasst, was zur Auswertung der Spracheingaben nötig ist wie etwa die commands und Grammatik, die die Spracherkennung dann mit dem Input vergleicht
 
             Console.Read();
 
         }
 
+        //Die Methode sucht in der Befehlsliste nach dem erkannten Kommando, um dann die zugehörige Antwort in der Antwortliste zu erfassen
         private void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             Console.WriteLine(e.Result.Text);
@@ -79,6 +80,8 @@ namespace KI_test_01
                 file.Close();
             }
         }
+
+        //Fügt die Textelemente als neues Listenelement zur jeweiligen Liste an
         static private void genLists(String Line, List<string> Bef, List<string> Ans)
         {
             int index = Line.IndexOf(";");
